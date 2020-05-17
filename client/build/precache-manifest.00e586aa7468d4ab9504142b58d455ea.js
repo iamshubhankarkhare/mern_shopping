53acd6ad9e699c8c665d1c32ca504cf4eab16e85
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-05-17 17:05:32 +0530

    deployment errors fixed

---
+++ client/build/precache-manifest.00e586aa7468d4ab9504142b58d455ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c7142a34130ddbc1860960f5690763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dea411a0817b8196c3b2",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "bdf729a2e1a946ae5f2d",
+    "url": "/static/css/main.c9db7cab.chunk.css"
+  },
+  {
+    "revision": "dea411a0817b8196c3b2",
+    "url": "/static/js/2.ac16f71b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ac16f71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf729a2e1a946ae5f2d",
+    "url": "/static/js/main.66af2e2e.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
